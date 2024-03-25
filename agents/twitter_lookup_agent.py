fe6d6d5a8ef70c02cbffaa00043f367c294df590
aderemilekun <adelefatai@yahoo.com>
2024-03-25 14:57:57 +0100

    Twitter scaping implemented

---
+++ agents/twitter_lookup_agent.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
+
+from langchain_core.tools import Tool
+from tools.tools import get_profile_url
+
+
+def lookup(name: str) -> str:
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """given the full name {name_of_person} I want you to get me a link to their Twitter profile page, and extract from there their username.
+                    Your answer should only contain the person's username"""
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 Twitter profile page",
+            func=get_profile_url,
+            description="use for when you need to get the Twitter page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    return result["output"]
--- ice_breaker.py
+++ ice_breaker.py
@@ -4,28 +4,34 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from third_parties.twitter import scrape_user_tweets
 
 if __name__ == "__main__":
-    print("hello langchain")
+    print("Hello Langchain")
 
     linkedin_profile_url = linkedin_lookup_agent(name="Eden marco udemy")
+    linkedin_data = scrape_linkedin_profile(profile_url=linkedin_profile_url)
+
+    twitter_username = twitter_lookup_agent(name="Elon musk")
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=100)
 
     summary_template = """
-        given the Linkedin information {information} about a person, I want you to create:
+        given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person, I want you to create:
         1. A short summary
-        2. Two interesting facts about time
+        2. Two interesting facts about them
+        3. A topic that may interest then
+        4. 2 creative Ice breakers to open a conversation with them
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["linkedin_information", "twitter_information"], template=summary_template
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(profile_url=linkedin_profile_url)
-
-    res = chain.invoke(input={"information": linkedin_data})
+    res = chain.invoke(input={"linkedin_information": linkedin_data, "twitter_information": tweets})
 
     print(f"Result {res}")
